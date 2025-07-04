98dc5df962b3199d2705818c4e9a4f2959e92760
rawals <rawals.info@gmail.com>
2025-07-04 03:34:48 -0400

    feat: implement Slack notification provider module

--- my-medusa-store/src/modules/slack/index.ts
+++ my-medusa-store/src/modules/slack/index.ts
@@ -1,1 +1,6 @@
-export { default } from "./service"; 
+import { ModuleProvider, Modules } from "@medusajs/framework/utils";
+import SlackNotificationProviderService from "./service";
+
+export default ModuleProvider(Modules.NOTIFICATION, {
+  services: [SlackNotificationProviderService],
+}); 
