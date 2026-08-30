3690e76fef98e0107ecf12e1c3342cec5e7fb1d7
sid111rawal <sid111rawal@gmail.com>
2026-08-30 04:15:56 -0300

    feat: integrate Microsoft Clarity tracking script into application layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -384,6 +384,19 @@ export default function RootLayout(props: { children: React.ReactNode }) {
             __html: JSON.stringify(localBusinessSchema),
           }}
         />
+        {/* Microsoft Clarity */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "yacrhanzm7");
+            `
+          }}
+        />
       </head>
       <body suppressHydrationWarning={true}>
         {/* Google Tag Manager (noscript) */}
